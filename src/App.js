671c5f9c3acfdee030eb3029f0de0fd119f352b0
Natalie Ramirez <61169728+natalieramirez22@users.noreply.github.com>
2022-07-11 22:14:11 -0400

    create rudimentary nav bar and body

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 
 function App() {
-  return <p>hello</p>;
+  const navbar = (
+    <ul>
+      <NavItems>
+        <li>about</li>
+        <li>groups</li>
+        <li>friends</li>
+        <li>profile</li>
+      </NavItems>
+    </ul>
+  );
+
+  return (
+    <>
+      <NavBar>{navbar}</NavBar>
+      <Body>
+        <p>welcome to uni meet</p>
+      </Body>
+    </>
+  );
 }
+
+const NavItems = styled.li`
+  display: flex;
+  justify-content: space-between;
+  list-style-type: none;
+  color: palevioletred;
+  font-size: 1.2em;
+`;
+
+const NavBar = styled.section`
+  padding: 2em;
+  background: papayawhip;
+`;
+
+const Body = styled.section`
+  padding: 10em;
+  background: lightyellow;
+`;
 
 export default App;
